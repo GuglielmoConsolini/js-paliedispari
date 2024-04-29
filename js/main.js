192d6dbd80b8307ebcbb013903cef424166e3194
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-29 14:21:52 +0200

    JS palindromo 2/2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,28 @@ if (verificaPalindromo(parolaInserita)) {
     console.log("La parola inserita non è palindroma.");
 }
 
+function verificaPalindromo(parolaInserita){
+    
+// lettera iniziale e finale della parola
+let inizio = 0
+let fine = parolaInserita.length - 1
 
+ while (inizio < fine){
+
+    //   se le lettere non combaciano la funzione è False
+    if (parolaInserita[inizio] != parolaInserita[fine]){
+
+        return false;
+    }
+
+//    contatore per avvicinare le lettere al centro    
+    inizio++;
+    fine--;
+ }
+
+ return true;
+
+}
 
 
 
