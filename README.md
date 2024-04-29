85609f78ec7924ac427d608d756f17d0cfb0b419
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-29 15:18:10 +0200

    JS pari o dispari 1/1

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ darà esito negativo.
 3 dentro la funzione dichiaro due variabili: la lettera di inizio e quella finale. Con un ciclo while (che faccio iterare fino a quando l'inizio sarà minore della fine ) metto a confronto le lettere man mano che si avvicinano al centro attraverso un contatore. 
 
 4 attraverso un if dentro la funzione se le lettere saranno diverse la funzione sarà false, altrimenti true.
+
+
+
+
+JS PARI E DISPARI:
+
+1 Creo 2 funzioni : una per generare un numero casauale da 1 a 5(sarà il numero del computer) e l'altra mi servirà per fare la somma tra il numero del computer e quello dell'utente, in seguito mi dirà se è pari o dispari.
+
+2 chiedo all'utente attraverso due prompt: 1 : il suo numero ; 2 : cosa sceglie fra pari e dispari.
+
+3 dichiaro che la variabile numeroComputer sia uguale al numero generato dalla funzione
+
+4 dichiaro inoltre che il risultato sarà in base all'esito della somma dei numeri dentro la funzione. Se la somma avrà un "return" pari il risultato sarà pari ecc..
+
+5 in console stampo un messaggio di vittoria se la scelta dell'utente sarà uguale al risultato , in caso contrario un messaggio di sconfitta.
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+// JS VERIFICA PAROLA PALINDROMA
+
 let parolaInserita = prompt("Inserisci una parola:");
 
 if (verificaPalindromo(parolaInserita)) {
@@ -7,7 +10,7 @@ if (verificaPalindromo(parolaInserita)) {
 }
 
 function verificaPalindromo(parolaInserita){
-    
+
 // lettera iniziale e finale della parola
 let inizio = 0
 let fine = parolaInserita.length - 1
@@ -28,6 +31,42 @@ let fine = parolaInserita.length - 1
  return true;
 
 }
+
+
+// JS PARI E DISPARI
+
+function numeroCasuale() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+function sommaPariDispari(n1, n2) {
+    let somma = n1 + n2;
+
+    if (somma % 2 === 0){
+        return "pari"
+    }
+
+    return "dispari"
+}
+
+let sceltaUtente = prompt("Scegli su cosa puntare: pari o dispari?");
+console.log(`Hai scelto: ${sceltaUtente}`)
+
+let numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5"))
+console.log(`Il tuo numero è: ${numeroUtente}`)
+
+let numeroComputer = numeroCasuale()
+console.log(`Il numero del computer è ${numeroComputer}`)
+
+let risultato = sommaPariDispari(numeroComputer,numeroUtente )
+console.log(`La somma è ${risultato}`)
+
+if(risultato === sceltaUtente){
+    console.log("Complimenti hai vinto")
+} else{
+    console.log("Mi dispiace, hai perso")
+}
+
 
 
 
